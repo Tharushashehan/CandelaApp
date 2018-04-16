f25f9c75cd0bdb32e5956928868de4e27b93e38e
DESKTOP-M3T0SOU\tharu <deshanpraveendra@gmail.com>
2018-04-16 17:07:20 +0530

    added selector fab

--- CandelaLearning/app/src/main/java/lk/candelalearning/candelalearning/mcqFragment.java
+++ CandelaLearning/app/src/main/java/lk/candelalearning/candelalearning/mcqFragment.java
@@ -8,7 +8,10 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,6 +45,8 @@ public class mcqFragment extends Fragment{
     int Marks = 0;
     int LastCorrectness = 35;
     CountDownTimer TheTimer;
+    FloatingActionButton fab_select;
+    int position;
     //Context context = getActivity();
     //MainActivity MA = new MainActivity();
 
@@ -52,6 +57,7 @@ public class mcqFragment extends Fragment{
         myView = inflater.inflate(R.layout.mcq, container, false);
         tv = (TextView) myView.findViewById(R.id.textView2);
         answer_list_view = (ListView) myView.findViewById(R.id.answer_list_view);
+        fab_select = (FloatingActionButton) myView.findViewById(R.id.fab_select);
         //MainActivity.getnext_button().setVisibility(View.GONE);
 
 
@@ -221,12 +227,13 @@ public class mcqFragment extends Fragment{
                     tv.setText("Please select a paper from of a year");
                     answer_list_view.setAdapter(adapter);
 
-                }else  if (Ativityposition > 2 & QuestionNumber <= 10){
+               /* }else  if (Ativityposition > 2 & QuestionNumber <= 10){*/
+                }else  if (Ativityposition == 3 & QuestionNumber <= 10){
                     if (Ativityposition == 3){
                         TheTimer = new CountDownTimer(30000, 1000) {
                             public void onTick(long millisUntilFinished) {
                                 //MainActivity.getTextViewTimer().setTextColor(Color.RED);
-                                MainActivity.getTextViewTimer().setText("Seconds remaining: " + millisUntilFinished / 1000);
+                                MainActivity.getTextViewTimer().setText("Seconds: " + millisUntilFinished / 1000);
                             }
                             public void onFinish() {
                                 MainActivity.getTextViewTimer().setText("Done");
@@ -271,7 +278,12 @@ public class mcqFragment extends Fragment{
                     }catch (Exception ex){
                         tv.setText("Click next to start");
                     }
-                }else if (QuestionNumber > 10){
+                }else if (QuestionNumber > 3){
+                    answer_list_view.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+                    answer_list_view.setSelector(android.R.color.holo_green_dark);
+                    answer_list_view.setSelector(android.R.color.background_dark);
+                }
+                else if (QuestionNumber > 10){
 
                     int index, Subject_Id;
                     String Subject, Phone = "0";
@@ -319,6 +331,98 @@ public class mcqFragment extends Fragment{
             }
         });
 
+
+        fab_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              /*  Snackbar.make(view, "Adding Candela Hotline to dialer", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Uri number = Uri.parse("tel:0117 670 650");
+                        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                        startActivity(callIntent);
+                    }
+                }, 2000);*/
+
+
+                    if (Ativityposition > 3 & QuestionNumber <= 10){
+
+
+                        MainActivity.getnext_button().setVisibility(View.VISIBLE);
+                        //ShowData SD = new ShowData();
+                        //adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item, SD.SetMCQStringArrayData( new DataBaseHelper(MainActivity.getAppContext()), Ativityposition));
+                        //Ativityposition++;
+                        //answer_list_view.setAdapter(adapter);
+                        ShowData SD = new ShowData();
+                        mcqFragment fragment =  SD.SetMCQFragmentData(new DataBaseHelper(MainActivity.getAppContext()), Ativityposition);
+
+                        try{
+                            int value1=0;
+                            String value2=" ";
+                            String[] StringArray = {};
+
+                            Bundle bundle = fragment.getArguments();
+                            if (bundle != null) {
+                                if (LastCorrectness==position){
+                                    Marks++;
+                                }
+                                value1 = bundle.getInt("VALUE1", -1);
+                                value2 = bundle.getString("VALUE2", "h");
+                                StringArray = bundle.getStringArray("VALUE3");
+                                LastCorrectness = bundle.getInt("VALUE4", -1);
+                                tv.setText("Q: " + QuestionNumber + ") " + value2);
+                                //ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.second_layout, StringArray);
+                                adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item, StringArray);
+                                MainActivity.GetProgressBar().setProgress(QuestionNumber*10);
+                                Ativityposition++;
+                                QuestionNumber++;
+                                answer_list_view.setAdapter(adapter);
+
+
+                            }else{
+                                tv.setText("Click next to start");
+                            }
+                        }catch (Exception ex){
+                            tv.setText("Click next to start");
+                        }
+                    }else if (QuestionNumber > 10){
+
+                        int index, Subject_Id;
+                        String Subject;
+                        MainActivity.getnext_button().setVisibility(View.GONE);
+
+                        tv.setText("Your result is");
+                        myDb = new DataBaseHelper(MainActivity.getAppContext());
+                        Cursor GradeCursor = myDb.getAllGradeSubjectData(5);
+                        ArrayList<String> TmpArryLst = new ArrayList<String>();
+
+                        try{
+                            while (GradeCursor.moveToNext()) {
+                                index = GradeCursor.getColumnIndexOrThrow("Subject_Id");
+                                Subject_Id = Integer.parseInt(GradeCursor.getString(index));
+
+                                index = GradeCursor.getColumnIndexOrThrow("Subject");
+                                Subject = GradeCursor.getString(index);
+
+                                //TmpArryLst.add(Subject);
+                            }
+                        }finally {
+                            GradeCursor.close();
+                        }
+
+                        //String[] ResultArray = new String[TmpArryLst.size()];
+                        //ResultArray = TmpArryLst.toArray(ResultArray);
+                        String[] ResultArray = {Integer.toString(Marks) + " out of 10"};
+                        adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item, ResultArray);
+                        answer_list_view.setAdapter(adapter);
+                    }
+
+
+            }
+        });
         return  myView;
         //return super.onCreateView(inflater, container, savedInstanceState);
     }
